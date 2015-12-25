9444f1afb2b9ad2cb7bad7aea872e592d5d2143e
kitty jutbring <kittyj@cmail.nu>
2015-12-25 23:45:07 +0100

    change to use couchdb

--- locationRoutes.js
+++ locationRoutes.js
@@ -1,5 +1,18 @@
+var parse = require("co-body");
 
+var nano = require("nano")("http://localhost:5984");
+var wrap = require("co-nano");
+var coNano = require("co-nano")(nano);
+
+var db = coNano.use("location_api");
+
+module.exports.add = function * () {
+    var postedLocation = yield parse(this);
+    
+    var res = yield db.insert(postedLocation);
+    var body = res[0], headers = res[1];
+};
 
 module.exports.get = function *get(device) {
     this.body = "you passed: " + device;
-}
+};
