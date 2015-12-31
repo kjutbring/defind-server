89db8ab3e9d9fbebc177afb20494276a4ee49fcc
kitty jutbring <kittyj@cmail.nu>
2015-12-31 03:31:19 +0100

    remove co-nano and use nano

--- locationRoutes.js
+++ locationRoutes.js
@@ -1,30 +1,36 @@
 var parse = require("co-body");
 
-var nano = require("nano")("http://localhost:5984");
-var wrap = require("co-nano");
-var coNano = require("co-nano")(nano);
-
-var db = coNano.use("location_api");
+var nano = require("nano")("http://localhost:5984/location_api");
 
 module.exports.add = function * () { 
 
     var postedLocation = yield parse(this);
     
-    var res = yield db.insert(postedLocation);
-    var body = res[0], headers = res[1];
+    console.log(postedLocation);
+     
+    nano.insert(postedLocation, function(err, body) {
+       
+        if (!err) {
+            console.log(body);
+        }
+    });
 
-    this.set("locaton", "/api/location/" + res._id);
-    this.status = 200;      
-    
+    this.status = 200; 
 };
 
 module.exports.get = function *get(device) {
   
-    var deviceJson = { device: device };
+    var deviceJson = { device: [device] };
+    
+    nano.view("locations", "by_device_id", deviceJson, function(err, body) {
+        
+        if (!err) {
+            body.rows.forEach(function(doc) {
+                console.log(doc);
+            });
+        }
+    });
 
-    var res = yield db.fetch(deviceJson);
-
-    this.body = res;
     this.status = 200;
 };
 
